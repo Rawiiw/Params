cf82a6219cdf77446b45f88da6e1f33dee826c6e
KosinovaAi <KosinovaAi@201D-11>
2022-10-06 17:54:59 +0700

    Добавьте файлы проекта.

---
+++ Params/Point.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "params.h"
+
+class Point
+{
+	int x;
+	int y;
+	for (x=0; x = y; x++)
+	for (y=0; y=x;y++)
+	
+};
---
+++ Params/params.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+class Params
+{
+	int x; //координата х
+	int y; //координата y
+
+public:
+	void GetSize();
+	class Point;
+	class Line;
+	class Circle;
+	class Square;
+};
+
